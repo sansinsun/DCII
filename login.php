a07192761a1499f5eeae6f6d1c55974ecbb2597f
sansinsun <fadhilah.insan21@gmail.com>
2020-10-06 16:37:36 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,36 @@
+<link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="css/style.css" rel="stylesheet" id="bootstrap-css">
+
+<script src="js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="img/logo.jpg" id="icon" alt="User Icon" />
+    </div>
+    <div class="fadeIn first">
+        SIGN IN TO YOUR ACCOUNT
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="example@email.com">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+      <div id="formFooter">
+  	      	<a class="underlineHover" href="#">Forgot Password?</a>
+  	      </div>
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <!-- <div id="formFooter">
+      <a class="underlineHover" href="#">Forgot Password?</a>
+    </div> -->
+
+  </div>
+</div>
---
+++ nav.php
@@ -0,0 +1,48 @@
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon">
+        <i class="fas fa-user-circle"></i>
+    </div>
+  <div class="sidebar-brand-text mx-3">Building Manager</div>
+</a>
+ <!-- Nav Item - Pages Collapse Menu -->
+ <!-- Nav Item - Dashboard -->
+ <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Interface
+      </div>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-hdd"></i>
+          <span>Components</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="py-2 collapse-inner">
+            <a class="collapse-item" href="component_list.php">List</a>
+            <a class="collapse-item" href="cards.html">Add New Component</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-calendar"></i>
+          <span>Event</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="py-2 collapse-inner rounded">
+            <a class="collapse-item text-menu" href="event_list.php">Event History</a>
+            <a class="collapse-item" href="utilities-border.html">Add Event</a>
+          </div>
+        </div>
+      </li>
+    </ul>
+<!-- End of Sidebar -->
+
+  
